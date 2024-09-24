8af44d2125f2f2781d16927bb800c7d105615a46
Capella <mcur99.03@gmail.com>
2024-09-24 16:25:27 -0500

    primer commit

---
+++ src/controller/loginController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+class LoginController{
+    // inicion de sesión de las personals
+       static async loginPersona(req:Request, res:Response){
+            const { tipoId, nomeroid, fechanac } = req.body;
+            try {
+                await 
+            } catch (error) {
+                
+            }
+       }
+    }
+    
